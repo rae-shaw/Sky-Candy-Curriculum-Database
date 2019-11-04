1a0715fd158c7180466f6f7369dcf79789457a9d
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-11-04 13:00:36 -0600

    updated favicon

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -15,7 +15,7 @@ export default function(){
 				<h1>Welcome to the Sky Candy Curriculum Database!</h1>
 			</header>
 			<main className = 'landing'>
-				<p>To search for a skill or skills or to edit an existing skill, click 'Search.' To add a skill, click 'Add Skill'.</p>
+				<p>Welcome to the Sky Candy Curriculum Database. This database is designed to hold all of the curriculum for Sky Candy classes for reference, for adding new skills, updating current skills or deleting outdated skills. To search for a skill or skills or to edit or delete an existing skill, click 'Search.' To add a skill, click 'Add Skill'.</p>
 				<section className='buttonRow'>
 					<Link to = '/search'>
 						<button className ='buttons'>Search</button>
